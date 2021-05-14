124d304abacf7b36e495ad97714646f1c1c00671
Сергей Волегов <sergey.volegov@playrix.com>
2021-05-14 10:53:48 +0500

    Резервное сохранение

--- scripts/game_engine.js
+++ scripts/game_engine.js
@@ -15,7 +15,12 @@ class Game
 		this.context = this.canvas.getContext("2d");
 		this.block = document.getElementById(id);
 		
-		var arr = ['keydown', 'keyup', 'mousedown', 'mouseup'];
+		/* Init classes */
+		Camera.init(this.canvas)
+		Input.init(this.canvas)
+		
+		/* Init events */
+		var arr = ['keydown', 'keyup', 'mousedown', 'mouseup', "mousemove"];
 		for(var i in arr)
 		{
 			document.body.addEventListener(arr[i], Input.handleEvent);
@@ -62,7 +67,7 @@ class Game
 		{
 			this.started = true;
 			var self = this;
-			setTimeout(function(){self.loop()}, 1000 / this.fps);
+			setTimeout(() => {this.loop()}, 1000 / this.fps);
 		}
 	}
 	
@@ -74,30 +79,18 @@ class Game
 			Input.update()
 			
 			this.update()
-			this.draw()
 
-			var self = this;
-			setTimeout(function(){self.loop()}, 1000 / this.fps);
+			setTimeout(() => {this.loop()}, 1000 / this.fps);
 		}
-	}
-	
-	clear()
-	{
-		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 	}
 	
 	update()
 	{
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 		for(var i in this.entities) 
 		{
 			this.entities[i].update();
 		}
-	}
-	
-	draw()
-	{
-		this.clear()
-		for(var i in this.entities) this.entities[i].draw(this.context);
 	}
 }
 
@@ -111,15 +104,6 @@ class Entity
 	constructor(name = null)
 	{
 		this.name = name;
-	}
-	
-	draw(context)
-	{
-		if(this.hasComponent("DrawableComponent"))
-		{
-			let visual_component = this.getComponent("DrawableComponent");
-			visual_component.draw(context)
-		}
 	}
 	
 	init()
@@ -154,64 +138,6 @@ class Entity
 	}
 }
 
-/* Time class */
-class Time
-{
-	static date = new Date();
-	static delta_time = 0;
-	
-	static update()
-	{
-		let time = this.date.getTime()
-		Time.date = new Date();
-		Time.delta_time = (this.date.getTime() - time) / 1000;
-	}
-}
-
-/* Input handler class */
-class Input
-{
-	static mouse_clicked = {};
-	static keyboard_clicked = {};
-	
-	static update()
-	{
-		for(let key in Input.mouse_clicked) Input.mouse_clicked[key] = false;
-		for(let key in Input.keyboard_clicked) Input.mouse_clicked[key] = false;
-	}
-	
-	static handleEvent(event)
-	{		
-		switch(event.type)
-		{
-			case "keydown": 	Input.keyboard_clicked[event.code] = true; break;
-			case "keyup": 		delete Input.keyboard_clicked[event.code]; break;
-			case "mousedown": 	Input.mouse_clicked[event.button] = true; break;
-			case "mouseup": 	delete Input.mouse_clicked[event.button]; break;
-		}
-	}
-	
-	static isMousePressed(button)
-	{
-		return Input.mouse_clicked[button] != undefined;
-	}
-	
-	static isMouseClicked(button)
-	{
-		return Input.mouse_clicked[button] == true;
-	}
-	
-	static isKeyPressed(button)
-	{
-		return Input.keyboard_clicked[button] != undefined;
-	}
-	
-	static isKeyClicked(button)
-	{
-		return Input.keyboard_clicked[button] == true;
-	}
-}
-
 /* --------------------------------- Other classes  -------------------------------------- */
 class Vector2
 {
@@ -236,7 +162,7 @@ class Vector2
 	
 	getDirection(point)
 	{
-		return Math.atan2(point.y - this.y, point.x - this.x) * 180 / Math.PI;
+		return Math.atan2(point.y - this.y, point.x - this.x) * 180 / Math.PI + 90;
 	}
 	
 	getVectorTo(point)
@@ -245,10 +171,12 @@ class Vector2
 		let y = point.y - this.y
 		let sum = Math.abs(x) + Math.abs(y);
 		
-		return new Vector2(x / sum, y / sum);
+		if (this.equals(point)) return new Vector2(0, 0);
+		else return new Vector2(x / sum, y / sum);
 	}
 }
 
+/* Color class */
 class Color
 {
 	r = 0;
@@ -267,6 +195,169 @@ class Color
 	toString()
 	{
 		return "rgba(" + this.r + "," + this.g + "," + this.b + "," + this.a + ")";
+	}
+}
+
+/* Time class */
+class Time
+{
+	static date = new Date();
+	static delta_time = 0;
+	
+	static update()
+	{
+		let time = this.date.getTime()
+		Time.date = new Date();
+		Time.delta_time = (this.date.getTime() - time) / 1000;
+	}
+}
+
+/* Camera class */
+class Camera
+{
+	static context = null;
+	static canvas = null;
+	static center = new Vector2(0, 0);
+	static angle = 0;
+	static zoom = 1.0
+	
+	static init(canvas)
+	{
+		Camera.canvas = canvas;
+		Camera.context = canvas.getContext("2d");
+	}
+	
+	static getPosition()
+	{
+		let size = Camera.getSize();
+		return new Vector2(Camera.center.x - size.x / 2, Camera.center.y - size.y / 2)
+	}
+	
+	static setCenter(point)
+	{
+		Camera.center = point
+	}
+	
+	static apply_transform()
+	{
+		if(Camera.context)
+		{
+			let size = Camera.getSize();
+			Camera.context.translate(-Camera.center.x + size.x / 2, -Camera.center.y + size.y / 2,)
+		}
+	}
+	
+	static getSize()
+	{
+		return new Vector2(Camera.canvas.width, Camera.canvas.height)
+	}
+}
+
+/* Resources class */
+class Resources
+{
+	static textures = {}
+	static loading_counter = 0;
+	
+	static isLoaded()
+	{
+		return Resources.loading_counter == 0;
+	}
+	
+	static loadTexture(name, src)
+	{
+		Resources.loading_counter += 1;
+		
+		Resources.textures[name] = new Image();
+		Resources.textures[name].src = src;
+		Resources.textures[name].onload = function()
+		{
+			Resources.loading_counter -= 1;
+		}
+	}
+	
+	static getTexture(name)
+	{
+		return Resources.textures[name];
+	}
+}
+
+/* Input handler class */
+class Input
+{
+	static canvas = null;
+	static mouse_pos = new Vector2(0, 0);
+	static mouse_clicked = {};
+	static keyboard_clicked = {};
+	
+	static init(canvas)
+	{
+		Input.canvas = canvas;
+	}
+	
+	static update()
+	{
+		for(let key in Input.mouse_clicked) Input.mouse_clicked[key] = false;
+		for(let key in Input.keyboard_clicked) Input.mouse_clicked[key] = false;
+	}
+	
+	static handleEvent(event)
+	{		
+		switch(event.type)
+		{
+			case "keydown": 	Input.keyboard_clicked[event.code] = true; break;
+			case "keyup": 		delete Input.keyboard_clicked[event.code]; break;
+			case "mousedown": 	Input.mouse_clicked[event.button] = true; break;
+			case "mouseup": 	delete Input.mouse_clicked[event.button]; break;
+			case "mousemove":  	if(Input.canvas) Input.mouse_pos = new Vector2(event.clientX-Input.canvas.offsetLeft,event.clientY-Input.canvas.offsetTop); break;
+		}
+	}
+	
+	static getGlobalMouse()
+	{
+		let point = Camera.getPosition()
+		return new Vector2(Input.mouse_pos.x + point.x, Input.mouse_pos.y + point.y);
+	}
+	
+	static getLocalMouse()
+	{
+		return Input.mouse_pos;
+	}
+	
+	static isMousePressed(button)
+	{
+		return Input.mouse_clicked[button] != undefined;
+	}
+	
+	static isMouseClicked(button)
+	{
+		return Input.mouse_clicked[button] == true;
+	}
+	
+	static isKeyPressed(button)
+	{
+		return Input.keyboard_clicked[button] != undefined;
+	}
+	
+	static isKeyClicked(button)
+	{
+		return Input.keyboard_clicked[button] == true;
+	}
+	
+	static isKeysPressed(arr, and=false)
+	{
+		let result = false;
+		if(arr.length > 0)
+		{
+			result = Input.isKeyPressed(arr[0]);
+			
+			for(let i = 1; i<arr.length; i++) 
+			{
+				if(and) result = Input.isKeyPressed(arr[i]) && result;
+				else result = Input.isKeyPressed(arr[i]) || result;
+			}
+		}
+		return result;
 	}
 }
 
@@ -353,6 +444,11 @@ class TransformComponent extends ComponentBase
 		return this.position;
 	}
 	
+	getCenter()
+	{
+		return new Vector2(this.position.x+this.size.x/2, this.position.y+this.size.y/2)
+	}
+	
 	getSize()
 	{
 		return this.size;
@@ -376,14 +472,11 @@ class TransformComponent extends ComponentBase
 	
 	move_to(point, speed)
 	{	
-		if(!this.position.equals(point))
-		{
-			let len = this.position.getDistance(point)
-			let vector = this.position.getVectorTo(point)
-			
-			if(len <= speed) this.setPosition(point);
-			else this.move(vector.x * speed, vector.y * speed);
-		}
+		let len = this.position.getDistance(point)
+		let vector = this.position.getVectorTo(point)
+		
+		if(len <= speed) this.setPosition(point.x, point.y);
+		else this.move(vector.x * speed, vector.y * speed);
 	}
 	
 	move_around(x, y, angle)
@@ -410,38 +503,24 @@ class TransformComponent extends ComponentBase
 	
 	rotate_at(point)
 	{
-		let angle = this.position.getDirection(point)
+		let center = this.getCenter()
+		let angle = center.getDirection(point)
 		this.setAngle(angle)
 	}
 }
 
 class DrawableComponent extends ComponentBase
 {	
-	shape = null
-	visible = true
+	fill_color = new Color(255, 255, 255)
+	stroke_color = new Color(0, 0, 0)
+	line_width = 0.0;
 	opacity = 1.0;
-	
-	draw(context)
-	{
-		if(this.visible && this.opacity > 0.0 && this.shape) this.shape.draw(context)
-	}
-	
-	setShape(value)
-	{
-		this.shape = value;
-	}
+	context = null;
 	
 	getOpacity()
 	{
 		return this.opacity
 	}
-}
-
-class ShapeComponent extends DrawableComponent
-{	
-	fill_color = new Color(255, 255, 255)
-	stroke_color = new Color(0, 0, 0)
-	line_width = 1.0;
 	
 	setFillColor(color)
 	{
@@ -457,41 +536,91 @@ class ShapeComponent extends DrawableComponent
 	{
 		this.line_width = value
 	}
-}
-
-class RectShapeComponent extends ShapeComponent
-{	
-	init()
-	{	
-		this.join("TransformComponent")
-		this.join("DrawableComponent").setShape(this)
+	
+	applyStyles()
+	{
+		this.context.globalAlpha = this.opacity;
+		this.context.fillStyle = this.fill_color;
+		this.context.strokeStyle = this.stroke_color;
+		this.context.lineWidth = this.line_width;
 	}
 	
-	draw(context)
-	{		
-		/* Get data */
+	applyTransformation()
+	{
 		let transform_component = this.joined["TransformComponent"]
 		let position = transform_component.getPosition()
 		let size = transform_component.getSize()
 		let angle = transform_component.getAngle()
 		let axis = transform_component.getAxis()
+		
+		Camera.apply_transform()
+		
+		this.context.translate(position.x + size.x * axis.x, position.y + size.y * axis.y)
+		this.context.rotate(Math.PI / 180 * angle);
+		this.context.translate(-size.x * axis.x - position.x, -size.y * axis.y - position.y)
+	}
+}
 
-		/* Style */
-		context.globalAlpha = this.opacity;
-		context.fillStyle = this.fill_color;
-		context.strokeStyle = this.stroke_color;
-		context.lineWidth = this.line_width;
-		
-		/* Transform */
-		context.translate(position.x, position.y)
-		context.translate(size.x * axis.x, size.y * axis.y)
-		context.rotate(Math.PI / 180 * angle);
-		context.translate(-size.x * axis.x, -size.y * axis.y)
-		
-		/* Draw */
-		context.fillRect(0, 0, size.x, size.y);
-		context.strokeRect(0, 0, size.x, size.y);
-		context.resetTransform();
+/* Rect shape */
+class RectShapeComponent extends DrawableComponent
+{	
+	init()
+	{	
+		this.join("TransformComponent")
+	}
+	
+	update()
+	{		
+		if(this.context && this.opacity > 0.0)
+		{
+			/* Get data */
+			let transform_component = this.joined["TransformComponent"]
+			let position = transform_component.getPosition()
+			let size = transform_component.getSize()
+			
+			/* Settings */
+			this.applyStyles();
+			this.applyTransformation()
+			
+			/* Draw */
+			this.context.fillRect(position.x, position.y, size.x, size.y);
+			if(this.line_width > 0.0) this.context.strokeRect(position.x, position.y, size.x, size.y);
+			
+			/* Reset*/
+			this.context.resetTransform();
+		}
+	}
+}
+
+class ImageComponent extends DrawableComponent
+{
+	texture = null;
+	
+	init()
+	{	
+		this.join("TransformComponent")
+	}
+	
+	update()
+	{		
+		if(this.context && this.texture && this.opacity > 0.0)
+		{	
+			/* Get data */
+			let transform_component = this.joined["TransformComponent"]
+			let position = transform_component.getPosition()
+			let size = transform_component.getSize()
+			
+			/* Settings */
+			this.applyStyles();
+			this.applyTransformation()
+			
+			/* Draw */
+			this.context.drawImage(this.texture, position.x, position.y, size.x, size.y);
+			if(this.line_width > 0.0) this.context.strokeRect(position.x, position.y, size.x, size.y);
+			
+			/* Reset*/
+			this.context.resetTransform();
+		}
 	}
 }
 
@@ -538,8 +667,22 @@ class WatcherComponent extends ComponentBase
 		{
 			let self_component = this.joined["TransformComponent"]
 			let target_component = this.target.getComponent("TransformComponent")
-			self_component.rotate_at(target_component.getPosition())
+			self_component.rotate_at(target_component.getCenter())
 		}
+	}
+}
+
+class MouseWatcherComponent extends ComponentBase
+{	
+	init()
+	{
+		this.join("TransformComponent")
+	}
+	
+	update()
+	{
+		let self_component = this.joined["TransformComponent"]
+		self_component.rotate_at(Input.getGlobalMouse())
 	}
 }
 
@@ -557,7 +700,6 @@ class RoundMovingComponent extends ComponentBase
 	{
 		let transform_component = this.joined["TransformComponent"]	
 		transform_component.move_around(this.axis.x, this.axis.y, this.speed * Time.delta_time)
-		if(this.rotating) transform_component.rotate_at(this.axis)
 	}
 }
 
@@ -566,6 +708,7 @@ class PursuerComponent extends ComponentBase
 	speed = 50;
 	target = null;
 	min_radius = 0;
+	middle_radius = 0;
 	max_radius = 100;
 	
 	init()
@@ -576,6 +719,7 @@ class PursuerComponent extends ComponentBase
 	update()
 	{
 		let target_transform = this.target.getComponent("TransformComponent")
+		
 		if(target_transform)
 		{
 			let transform_component = this.joined["TransformComponent"];
@@ -587,6 +731,7 @@ class PursuerComponent extends ComponentBase
 			let distance = self_pos.getDistance(target_pos)
 			
 			if(distance < this.min_radius) transform_component.move_to(target_pos, -speed)
+			else if(distance < this.middle_radius) return;
 			else if(distance < this.max_radius) transform_component.move_to(target_pos, speed)
 		}
 	}
@@ -594,8 +739,84 @@ class PursuerComponent extends ComponentBase
 
 class PlayerControlComponent extends ComponentBase
 {
-	controls = [];
+	controls = {};
+	acceleration = 2.0
+	running = false
+		
+	init()
+	{
+		this.join("TransformComponent")
+		
+		this.addControl("Run", ["ShiftLeft", "ShiftRight"]);
+		this.addControl("GoAhead", ["KeyW", "ArrowUp"]);
+		this.addControl("GoBack", ["KeyS", "ArrowDown"]);
+		this.addControl("GoLeft", ["KeyA", "ArrowLeft"]);
+		this.addControl("GoRight", ["KeyD", "ArrowRight"]);
+	}
 	
+	addControl(action, buttons)
+	{
+		this.controls[action] = buttons;
+	}
+	
+	update()
+	{
+		let transform_component = this.joined["TransformComponent"]
+		let speed = Time.delta_time * 50;
+		
+		/* Acceleration */
+		if(Input.isKeysPressed(this.controls["Run"])) 
+		{
+			this.running = true;
+			speed *= this.acceleration;
+		}
+		else this.running = false;
+		
+		/* Moving */
+		if(Input.isKeysPressed(this.controls["GoAhead"])) transform_component.move(0, -speed);
+		if(Input.isKeysPressed(this.controls["GoBack"])) transform_component.move(0, speed);
+		if(Input.isKeysPressed(this.controls["GoLeft"])) transform_component.move(-speed, 0);
+		if(Input.isKeysPressed(this.controls["GoRight"])) transform_component.move(speed, 0);
+	}
+}
+
+class LifeComponent extends ComponentBase
+{	
+	health_max = 100
+	health_value = this.health_max
+	
+	resurrect()
+	{
+		this.health_value = this.health_max
+	}
+	
+	kill()
+	{
+		this.health_value = 0
+	}
+	
+	isFine()
+	{
+		this.health_value == this.health_max; 
+	}
+	
+	isAlife()
+	{
+		this.health_value > 0;
+	}
+	
+	addHealth(value)
+	{
+		this.health_value += value
+		if(this.health_value > this.health_max) 
+		{
+			this.health_value = this.health_max
+		}
+	}
+}
+
+class CameraComponent extends ComponentBase
+{	
 	init()
 	{
 		this.join("TransformComponent")
@@ -604,14 +825,7 @@ class PlayerControlComponent extends ComponentBase
 	update()
 	{
 		let transform_component = this.joined["TransformComponent"]
-		let speed = Time.delta_time * 50;
-		
-		if(Input.isKeyPressed("KeyW")) transform_component.move(0, -speed);
-		if(Input.isKeyPressed("KeyS")) transform_component.move(0, speed);
-		if(Input.isKeyPressed("KeyA")) transform_component.move(-speed, 0);
-		if(Input.isKeyPressed("KeyD")) transform_component.move(speed, 0);
+		Camera.setCenter(transform_component.getCenter())
 	}
 }
-
-
 
